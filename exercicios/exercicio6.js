6227427157c5930cf06eee66e2a5d4d078ed9198
nicolasklange <nklange986@gmail.com>
2025-05-08 21:47:03 -0300

    exercicio 6 concluido

--- exercicios/exercicio6.js
+++ exercicios/exercicio6.js
@@ -1,6 +1,6 @@
 estaChovendo = false;
 
-if (estaChovendo == true) {
+if (estaChovendo) {
     console.log("Preciso levar um guarda chuva");
 } else {
     console.log("Não é preciso levar guarda chuva");
