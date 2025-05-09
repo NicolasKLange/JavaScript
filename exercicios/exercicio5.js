cf84fc09095ac1fd334e888e1afe4ef7e000474b
nicolasklange <nklange986@gmail.com>
2025-05-08 21:32:59 -0300

    exercicio 5 concluido

---
+++ exercicios/exercicio5.js
@@ -0,0 +1,17 @@
+var idade;
+
+if (idade) {
+    var idade;
+    console.log(idade)
+}
+
+console.log(idade)
+
+let idade;
+
+if (idade) {
+    let idade;
+    console.log(idade)
+}
+
+console.log(idade)
