6147f84d898a25275273800585a5ac9572085b49
nicolasklange <nklange986@gmail.com>
2025-05-08 21:20:29 -0300

    exercicio 2 concluido

---
+++ exercicios/exercicio2.js
@@ -0,0 +1,6 @@
+primeiroNome = "Nicolas";
+ultimoNome = "Lange";
+nomeCompleto = primeiroNome + ultimoNome;
+nomeTemplate = (`${primeiroNome} ${ultimoNome}`);
+console.log(nomeCompleto);
+console.log(nomeTemplate);
