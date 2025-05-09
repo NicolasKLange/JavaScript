e1a7445797d8158c1472d9a0657e1c13db9b06b5
nicolasklange <nklange986@gmail.com>
2025-05-08 21:32:46 -0300

    exercicio 1 concluido

--- exercicios/exercicio1.js
+++ exercicios/exercicio1.js
@@ -2,4 +2,4 @@ nome = "Nicolas"
 idade = 18
 maiorIdade = true
 
-console.log(`Nome: ${nome}, Idade: ${idade}, Maior de Idade? ${maiorIdade}`)
+console.log(`Nome: ${typeof nome}, Idade: ${typeof idade}, MaiorIdade: ${typeof maiorIdade}`)
