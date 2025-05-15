295454c5ee5eef5f2ceb773c9c9ddaf7e4da8373
nicolasklange <nklange986@gmail.com>
2025-05-15 20:00:56 -0300

    tipos de dados

---
+++ tipos.js
@@ -0,0 +1,17 @@
+const n1 = 8;
+const n2 = 6.3;
+const n3 = -2;
+const n4 = '5';
+
+// Number.parseInt -> Converte string para integer
+console.log('A média é ', (n1+n2+n3+Number.parseInt(n4))/4);
+console.log('b'+'a'+ +'a'+'a');
+console.log(n1*n4)
+
+//Numeros hexadecimais
+let numHex = 0XA;
+console.log(numHex);
+
+//Numeros octal
+let numOctal = 0o21;
+console.log(numOctal);
