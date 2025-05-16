2efff38c3238d835e56cdea900b79143138ec4c6
nicolasklange <nklange986@gmail.com>
2025-05-15 21:41:17 -0300

    exercicio 1 finalizado

---
+++ exercicio_aula3/exercicio1.js
@@ -0,0 +1,10 @@
+let saldo = 1000;
+let deposito = 300;
+let saque = 500;
+
+let operacaoDep = saldo + deposito;
+console.log("Saldo atual é",operacaoDep);
+
+
+let operacaoSaq = saldo - deposito;
+console.log("Saldo atual é",operacaoSaq);
